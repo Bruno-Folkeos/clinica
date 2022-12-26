a049756174757641e57983349f6e6f90162fdcc2
Otavio Duarte <devotaviomarianoduarte@hotmail.com>
2022-12-26 18:48:18 -0300

    Adciona modulo de contatos

---
+++ custom_addons/contatos/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'herança contatos',
+    'description':"""
+        O módulo adciona o campo de CPF para contatos.
+    """,
+    'version' : '1.0',
+    'category': 'Contatos',
+    'depends' : ['base', 'contacts'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
